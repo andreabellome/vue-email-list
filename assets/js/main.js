c53b415f616b10767b107a28c78eee30cf31437f
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-26 09:40:21 +0200

    api email

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,22 @@ createApp({
     data() {
       return {
         /* variables Vue 3 */
-        
+        email: ''
       }
+    },
+    created(){
+        
     },
     methods: {
         /* functions Vue 3 */
-        
+        attivaApi(){
+
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( res => {
+                    this.email = res.data.response
+                } )
+            }
+            
+        }
     }
 }).mount('#app')
